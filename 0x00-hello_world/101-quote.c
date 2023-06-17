0ea579ac520a6b2d8606e01da0e17e6fb5d1416d
wisdom <njimoguwisdom@gmail.com>
2023-06-16 17:30:25 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: returns one
+ */
+
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
