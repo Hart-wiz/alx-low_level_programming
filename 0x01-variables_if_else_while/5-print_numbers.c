5dad733670cf0cdf3604c23349c4e34efb1e9e22
wisdom <njimoguwisdom@gmail.com>
2023-06-19 00:37:51 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this the main funtion
+ *
+ * return: returns 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
