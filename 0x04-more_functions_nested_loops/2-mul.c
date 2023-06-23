546463bb048593967b10f92113b5641d0ccef2ad
wisdom <njimoguwisdom@gmail.com>
2023-06-23 14:06:13 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - this function multiplies the int
+ *
+ * @a: is one of the int
+ * @b: is one of the int
+ * Return: returns the multiples
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+this is the readme file for 0x04-more_functions_nested_loops
+
