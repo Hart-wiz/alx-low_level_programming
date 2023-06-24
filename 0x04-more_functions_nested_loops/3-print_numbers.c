a5f5fce5b07a1c1706f59079268b0e6abc8a30a3
wisdom <njimoguwisdom@gmail.com>
2023-06-24 13:26:28 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - this function prints numbers
+ *
+ *
+ */
+
+void print_numbers(void)
+{
+	int number;
+
+	for (number = 48; number <= 57; number++)
+	{
+		_putchar(number);
+	}
+
+	_putchar('\n');
+}
